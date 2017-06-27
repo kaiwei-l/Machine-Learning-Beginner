03aced881c87c64bc3835114b6b40e5cb73c9bf6
kevinnnluo <kevinnnluo@outlook.com>
2017-06-27 23:12:00 +1000

    Add files via upload

--- Decision-Tree/trees.py
+++ Decision-Tree/trees.py
@@ -1,7 +1,7 @@
 from math import log
 import operator
 
-
+"""Algorithm: ID3"""
 def createDataSet():
     dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
