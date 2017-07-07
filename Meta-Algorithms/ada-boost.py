65f9668dc8fb9a5604387d4f3de9bb760a179a78
kevinnnluo <kevinnnluo@outlook.com>
2017-07-07 22:59:00 +1000

    Add files via upload

--- Meta-Algorithms/ada-boost.py
+++ Meta-Algorithms/ada-boost.py
@@ -51,3 +51,25 @@ def buildStump(dataArr, classLabels, D):
 
 
 def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones(m, 1) / m)
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print("D: ", D.T)
+        alpha = float(0.5 * log((1.0 - error) / max(error, math.e - 16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print("classEst: ", classEst.T)
+        expon = multiply(-1 * alpha * mat(classLabels).T, classEst)
+        D = multiply(D, math.exp(expon))
+        D /= D.sum()
+        aggClassEst += alpha * classEst
+        print("aggClassEst: ", aggClassEst.T)
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m, 1)))
+        errorRate = aggErrors.sum() / m
+        print("total error: ", errorRate, "\n")
+        if errorRate == 0.0:
+            break
+    return weakClassArr
