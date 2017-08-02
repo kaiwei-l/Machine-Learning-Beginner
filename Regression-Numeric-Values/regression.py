1cef785e80ff48f0ac6ac5eba1410186f60531ce
kevinnnluo <kevinnnluo@outlook.com>
2017-08-02 12:41:49 +0800

    Add files via upload

---
+++ Regression-Numeric-Values/regression.py
@@ -0,0 +1,9 @@
+from numpy import *
+
+
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t')) - 1
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
